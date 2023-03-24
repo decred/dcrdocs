183ed40e77f9d5facf7c77c04fd8303948f3b851
jholdstock <jholdstock@decred.org>
2023-03-24 10:40:02 +0000

    build: Docker cleanup/update.
    - Use Alpine version of nginx image (23MB instead of 142MB)
    - Fix MIME type for rss feeds.
    - Clean up .gitignore and .dockerignore.
    - Ensure scripts fail on error.

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,7 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
 # Requires docker 17.05 or higher
+
+set -e
 
 echo ""
 echo "================================="
@@ -7,7 +9,13 @@ echo "  Building dcrdocs docker image  "
 echo "================================="
 echo ""
 
-docker build -t decred/dcrdocs .
+IMAGE_NAME=decred/dcrdocs
+
+if [ "$1" != "" ]; then
+    IMAGE_NAME=$1
+fi
+
+docker build -t $IMAGE_NAME .
 
 echo ""
 echo "==================="
@@ -15,5 +23,5 @@ echo "  Build complete"
 echo "==================="
 echo ""
 echo "You can now run dcrdocs with the following command:"
-echo "    docker run -d --rm -p <local port>:80 decred/dcrdocs:latest"
+echo "    docker run -d --rm -p <local port>:80 $IMAGE_NAME:latest"
 echo ""
